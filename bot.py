28a012cfb797e868b1e0b0eb6767698b06ed37f0
botuser <botuser@gmail.com>
2026-05-11 16:03:04 +0500

    first bot

---
+++ bot.py
@@ -0,0 +1,26 @@
+import telebot
+
+TOKEN= "8784214206:AAFYgqbF9GNfQFWgyxE6Rzu21VAlRLtV7FQ"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Assalomu alaykum! Murojaatingizni yuboring.")
+
+@bot.message_handler(func=lambda message: True)
+def forward_message(message):
+    admin_id = 8425724360
+    
+    text = f"""
+📩 Yangi murojaat
+
+👤 {message.from_user.first_name}
+🆔 {message.from_user.id}
+✉️ {message.text}
+"""
+
+    bot.send_message(admin_id, text)
+    bot.reply_to(message, "Xabaringiz yuborildi.")
+
+print("Bot ishga tushdi...")
+bot.infinity_polling()
