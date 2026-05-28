f16fd7a54153b09e9b1e0fbebf191c29628eda72
shahodat0507 <bakiyevashahodat@gmail.com>
2026-05-29 00:10:08 +0500

    fix

--- bot.py
+++ bot.py
@@ -1,14 +1,33 @@
 import telebot
+from flask import Flask
+from threading import Thread
+import os
 
 TOKEN = "8784214206:AAGVgFPVVGabD4A9Yl-RHHYKxlxWMIxcET0"
+
 bot = telebot.TeleBot(TOKEN)
 
 admin_id = 8425724360
 
 
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot ishlayapti!"
+
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+
+Thread(target=run).start()
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Assalomu alaykum")
+    bot.reply_to(
+        message,
+        "Assalomu alaykum! Xabaringizni yuboring."
+    )
 
 
 @bot.message_handler(func=lambda message: True)
@@ -26,17 +45,17 @@ def handle_message(message):
 
             bot.send_message(user_id, message.text)
 
-            bot.reply_to(message, "✅ Javob yuborildi")
+            bot.reply_to(message, "✅ Javob yuborildi!")
 
         except:
             bot.reply_to(
                 message,
-                "❌ Bu xabarga javob yuborib bo‘lmadi"
+                "❌ Bu xabarga javob yuborib bo‘lmadi."
             )
 
         return
 
-# User xabarini adminga forward qilish
+    # User xabarini adminga forward qilish
     bot.forward_message(
         admin_id,
         message.chat.id,
